bbb3a6b48b3a01541a555e63216dfb044e24452a
Grant <grant.cieslack@evsck12.com>
2021-09-21 13:53:50 -0500

    supernice got it

--- store.js
+++ store.js
@@ -60,7 +60,7 @@ function addToCartClicked(event) {
     var shopItem = button.parentElement.parentElement
     var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
     var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].id
     console.log(title, price, imageSrc)
     addItemToCart(title, price, imageSrc)
     updateCartTotal()
